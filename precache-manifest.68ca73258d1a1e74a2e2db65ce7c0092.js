d2ab4e6b847f5198776c302100d269d519d78435
Rahul Rana <3rahul4@gmail.com>
2019-04-28 23:09:50 +0530

    Updates

---
+++ precache-manifest.68ca73258d1a1e74a2e2db65ce7c0092.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "78aeebb464f7820b3b21",
+    "url": "/static/css/main.fa6730fb.chunk.css"
+  },
+  {
+    "revision": "78aeebb464f7820b3b21",
+    "url": "/static/js/main.64ab14d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0fa1fa97e2fbd32aab2",
+    "url": "/static/js/2.00cae8e3.chunk.js"
+  },
+  {
+    "revision": "40d7fa34e465399c36ca5bcf2b5ddf4b",
+    "url": "/static/media/linkedin-logo.40d7fa34.svg"
+  },
+  {
+    "revision": "f9a437782e4a68164f1ebf0623784a3c",
+    "url": "/static/media/ocean-bg.f9a43778.jpg"
+  },
+  {
+    "revision": "befdc7145635ec591cd4c33607bd41d2",
+    "url": "/static/media/profile-pic.befdc714.jpeg"
+  },
+  {
+    "revision": "ea056d93940037b0d814a82d8cdb098f",
+    "url": "/static/media/instagram-logo.ea056d93.svg"
+  },
+  {
+    "revision": "8ee542201f49b258001d942cb280cbed",
+    "url": "/static/media/skype-logo.8ee54220.svg"
+  },
+  {
+    "revision": "43628cf70654a6367e61765a4fa3f62d",
+    "url": "/index.html"
+  }
+];
